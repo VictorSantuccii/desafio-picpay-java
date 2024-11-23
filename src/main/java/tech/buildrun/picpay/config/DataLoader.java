19f2d30ac5f061d34c62ab430462b05e7d4f4859
VictorSantuccii <victorfregne@gmail.com>
2024-11-23 19:43:39 -0300

    feat(entity): creation of wallet and walletType entities

---
+++ src/main/java/tech/buildrun/picpay/config/DataLoader.java
@@ -0,0 +1,14 @@
+package tech.buildrun.picpay.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+    
+}
---
+++ src/main/java/tech/buildrun/picpay/entity/WalletType.java
@@ -0,0 +1,55 @@
+package tech.buildrun.picpay.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_wallet_type")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WalletType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+
+    @Getter
+    @AllArgsConstructor
+    public enum Enum {
+        USER(1L, "user"),
+        MERCHANT(2L, "merchant");
+
+        private final Long id;
+        private final String description;
+
+        public WalletType get() {
+            return new WalletType(id, description);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        WalletType that = (WalletType) o;
+        return Objects.equals(id, that.id) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
